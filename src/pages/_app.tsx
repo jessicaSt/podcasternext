d1734f47e094b3f2bb8815b7f5eadab034dc5cbe
Jéssica Staehler <jessica@basedev.com.br>
2021-04-23 21:28:02 -0300

    Aula 4

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,18 +2,40 @@ import '../styles/global.scss'
 
 import { Header } from '../components/Header'
 import { Player } from '../components/Player';
+import { PlayerContext } from '../constexts/PlayerContext';
 
 import styles from '../styles/app.module.scss';
+import React, { useState } from 'react';
 
 function MyApp({ Component, pageProps }) {
+  const [episodeList, setEpisodeList] = useState([]);
+  const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0);
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  function play(episode) {
+    setEpisodeList([episode]);
+    setCurrentEpisodeIndex(0);
+    setIsPlaying(true);
+  }
+
+  function togglePlay() {
+    setIsPlaying(!isPlaying);
+  }
+
+  function setPlayngState(state: boolean) {
+    setIsPlaying(state);
+  }
+
   return (
-    <div className={styles.wrapper}>
-      <main>
-        <Header />
-        <Component {...pageProps} />
-      </main>
-      <Player />
-    </div>
+    <PlayerContext.Provider value={{ episodeList, currentEpisodeIndex, play, isPlaying, togglePlay, setPlayngState }}>
+      <div className={styles.wrapper}>
+        <main>
+          <Header />
+          <Component {...pageProps} />
+        </main>
+        <Player />
+      </div>
+    </PlayerContext.Provider>
   )
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,15 @@
+import { useContext } from "react";
 import { GetStaticProps } from "next";
 import Image from 'next/image';
 import Link from 'next/link'
 import { format, parseISO } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
+
 import { api } from "../services/api";
 import { convertDurationToTimeString } from "../utils/convertDurationToTimeString";
+import { PlayerContext } from "../constexts/PlayerContext";
+
 import styles from './home.module.scss';
-import React from "react";
 
 type Episode = {
   id: string;
@@ -26,10 +29,13 @@ type HomeProps = {
 }
 
 export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
+  const { play } = useContext(PlayerContext)
+
   return (
     <div className={styles.homepage}>
       <section className={styles.latestEpisodes}>
         <h2>Últimos lançamentos</h2>
+
         <ul>
           {latestEpisodes.map(episode => {
             return (
@@ -53,7 +59,7 @@ export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
                   <span>{episode.durationAsString}</span>
                 </div>
 
-                <button type="button">
+                <button type="button" onClick={() => play(episode)}>
                   <img src="play-green.svg" alt="Tocar episódio" />
                 </button>
               </li>
